89ff93b0418bf37302c24be3b341cb1af325ebf7
BekzodJumabaev <bekzodjumabaev04@gmail.com>
2024-11-04 11:29:52 +0500

    Added method's TodoService

--- src/Todo.java
+++ src/Todo.java
@@ -1,6 +1,53 @@
+import java.time.LocalTime;
+import java.util.UUID;
+
 public class Todo {
     private String id;
     private String todoName;
-    private String addedTime;
+    private LocalTime addedTime;
+
+    public Todo() {
+        this.id = UUID.randomUUID().toString().replace('-',  '.');
+    }
+
+    public Todo(String todoName) {
+        this();
+        LocalTime localDate = LocalTime.now();
+        this.todoName = todoName;
+        this.addedTime = localDate;
+    }
+
+    public String getTodoName() {
+        return todoName;
+    }
+
+    public void setTodoName(String todoName) {
+        this.todoName = todoName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalTime getAddedTime() {
+        return addedTime;
+    }
+
+    public void setAddedTime(LocalTime addedTime) {
+        this.addedTime = addedTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id='" + id + '\'' +
+                ", todoName='" + todoName + '\'' +
+                ", addedTime=" + addedTime +
+                '}';
+    }
 
 }
--- src/TodoService.java
+++ src/TodoService.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoService {
-    
+
+    List<Todo> todoList;
+
+    public TodoService() {
+        this.todoList = new ArrayList<>();
+    }
+
+    public boolean createTodo(String todoName){
+        boolean add = false;
+        Todo todo = new Todo(todoName);
+        add = todoList.add(todo);
+        return add;
+    }
+
+    public void updateTodo(String todoId, String newTodoName){
+        for (Todo todo : todoList) {
+            if (todo.getId().equals(todoId)){
+                todo.setTodoName(newTodoName);
+            }
+        }
+    }
+
+    public boolean deleteTodo(String todoId) {
+        boolean remove = false;
+        for (Todo todo : todoList) {
+            if (todo.getId().equals(todoId)) {
+                remove = todoList.remove(todo);
+            }
+        }
+        return remove;
+    }
+
+    public List<Todo> getTodo(){
+        return todoList;
+    }
 }
